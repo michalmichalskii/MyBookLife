a5e03dbb281a47c669bee28e87c8a594a54da28a
Michal Michalski <michmichal2002@gmail.com>
2024-12-04 13:42:54 +0100

    Add sign in with google acc

--- MyBookLife.Web/Program.cs
+++ MyBookLife.Web/Program.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
 using MyBookLife.Application;
 using MyBookLife.Infrastructure;
 
@@ -12,7 +13,13 @@ namespace MyBookLife.Web
             var builder = WebApplication.CreateBuilder(args);
 
             // Add services to the container.
+            var configuration = builder.Configuration;
             var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
+            builder.Services.AddAuthentication().AddGoogle(googleOptions =>
+            {
+                googleOptions.ClientId = configuration["Authentication:Google:ClientId"];
+                googleOptions.ClientSecret = configuration["Authentication:Google:ClientSecret"];
+            });
             builder.Services.AddDbContext<Context>(options =>
                 options.UseSqlServer(connectionString));
             builder.Services.AddDatabaseDeveloperPageExceptionFilter();
