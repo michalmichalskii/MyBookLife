33cb78dac89cc14a07e51972a933029ca1bf8f08
Michal Michalski <michmichal2002@gmail.com>
2024-12-05 16:33:22 +0100

    add admin panel

---
+++ MyBookLife.Web/Controllers/AdministrationController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MyBookLife.Web.Controllers
+{
+    [Authorize (Roles = "Admin")]
+    public class AdministrationController :Controller
+    {
+        private readonly UserManager<IdentityUser> _userManager;
+
+        public AdministrationController(UserManager<IdentityUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        [HttpGet]
+        public IActionResult GetListUsers()
+        {
+            var users = _userManager.Users;
+            return View(users);
+        }
+    }
+}
